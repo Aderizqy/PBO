2a3f57dda9b5f8a08537fa8e92aad57925e67b80
Aderizqy <aderizqy99@gmail.com>
2025-04-06 11:17:27 +0700

    first commit

---
+++ src/tugas/Node.java
@@ -0,0 +1,23 @@
+package tugas;
+
+public class Node {
+    private int value;
+    private Node node;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
+}
+
---
+++ src/tugas/Persistence.java
@@ -0,0 +1,7 @@
+package tugas;
+
+public class Persistence {
+    public void save(Lingkaran obj) {
+        System.out.println("Menyimpan objek dengan jari-jari: " + obj.getJari2());
+    }
+}
